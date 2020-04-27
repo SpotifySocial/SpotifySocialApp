a8ad1ff3afe2492cd82b65352af844936bf17c6b
Sidhaant Anand <=>
2020-04-28 00:51:42 +0530

    G#2: Sohini Punctuation fix

--- back/src/app.js
+++ back/src/app.js
@@ -12,9 +12,9 @@ app.use(bodyParser.json())
 app.use(session({secret: 'secret'}));
 
 const server = app.listen(process.env.PORT || 8080, () => { 
-	if(!process.env.SPOTIFY_CLIENT_ID || !process.env.SPOTIFY_CLIENT_SECRET) {;
+	if(!process.env.SPOTIFY_CLIENT_ID || !process.env.SPOTIFY_CLIENT_SECRET) {
 		server.close();
 		return;
 	}
-	console.log(`Listening on port ${process.env.PORT || 8080}!`);
+	console.log(`Listening on port ${process.env.PORT || 8080}`);
 });
