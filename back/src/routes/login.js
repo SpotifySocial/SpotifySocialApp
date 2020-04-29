4ba0c6b785b66aa804b063e5baa03475fcfa0857
Sidhaant Anand <=>
2020-04-29 11:02:39 +0530

    G#2 Added routes for login and redirect

---
+++ back/src/routes/login.js
@@ -0,0 +1,11 @@
+module.exports = function(req,res,constants,querystring) {
+	res.cookie(constants.stateCookie, process.env.AUTH_COOKIE_VAL);
+  	res.redirect(constants.spotifyAuthUrl +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: constants.authScope,
+      redirect_uri: constants.backendRedirectDomain + constants.backendRedirectRoute,
+      state:  process.env.AUTH_COOKIE_VAL
+    }));
+}
---
+++ back/src/routes/redirect.js
@@ -0,0 +1,35 @@
+module.exports = function(req,res,constants,request) {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const stateSecret = req.cookies ? req.cookies[constants.stateCookie] : null;
+
+  if (state === null || stateSecret !== process.env.AUTH_COOKIE_VAL) {
+    res.redirect(401,constants.appRedirectDomain+constants.appErrorRedirectRoute);
+    return;
+  } else {
+    res.clearCookie(constants.stateCookie);
+    const authOptions = {
+      url: constants.spotifyTokenUrl ,
+      form: {
+        code: code,
+        redirect_uri: constants.backendRedirectDomain + constants.backendRedirectRoute,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, function(error, response, body) {
+	  if(error || response.statusCode != 200){
+	  	res.redirect(401,constants.appRedirectDomain+constants.appErrorRedirectRoute);
+	  	return;
+	  }
+    
+    	req.session.access_token = body.access_token;
+    	req.session.refresh_token = body.refresh_token;
+      res.redirect(200,constants.appRedirectDomain+constants.appOkRedirectRoute);
+    });
+  }
+}
