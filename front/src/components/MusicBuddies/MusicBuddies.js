6aa1530797ae739937a77892b3845f7075bc21a5
Sohini M <smukherjee@Sohinis-MacBook-Pro.local>
2020-04-28 21:40:19 -0700

    G#2: Moved from canvasjs to chartjs since the former isn't open source.

--- front/src/components/MusicBuddies/MusicBuddies.js
+++ front/src/components/MusicBuddies/MusicBuddies.js
@@ -1,54 +1,67 @@
 import React from 'react';
-import CanvasJSReact from '../../canvasjs.react';
+import {HorizontalBar} from 'react-chartjs-2';
+import 'chartjs-plugin-datalabels';
 
 import './MusicBuddies.scss';
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
 
 function MusicBuddies() {
 
-  const options = {
-    animationEnabled: true,
-    backgroundColor: null,
-    axisX: {
-      labelFontColor: "#ecebe8",
-      lineColor: "transparent",
-      reversed:  true,
-      tickThickness: 0
-    },
-    axisY: {
-      gridThickness: 0,
-      lineColor: "transparent",
-      maximum: 100,
-      tickThickness: 0,
-      labelFormatter: function() {
-        return " ";
+  const buddies = {
+    labels: ['Jane', 'John', 'Jill',
+             'Jess', 'Jack'],
+    datasets: [
+      {
+        backgroundColor: '#62d36e',
+        data: [76, 65, 44, 32, 16],
       }
-    },
-    dataPointWidth: 40,
-    data: [{
-      type: "bar",
-      color: "#62d36e",
-      indexLabel: "{y}%",
-      indexLabelFontColor: "#ecebe8",
-      toolTipContent: "{label}: {y}%",
-      dataPoints: [
-          { y: 78, label: "Jane" },
-          { y: 65, label: "John" },
-          { y: 44, label: "Jill" },
-          { y: 32, label: "Jess" },
-          { y: 16, label: "Jack" }
-      ]
-    }]
-   }
+    ]
+  }
 
    return (
       <div class="music-buddies">
         <p className="music-buddies--header">
           Your top music buddy is Jane Doe who has a 76% similarity quotient
         </p>
-        <div class="music-buddies--chart">
-          <CanvasJSChart options = {options} />
+        <div>
+          <HorizontalBar
+            width={400}
+            height={350}
+            data={buddies}
+            options={{
+            legend:{
+              display:false,
+            },
+            scales: {
+              yAxes: [{
+                barPercentage: 0.5,
+                categoryPercentage: 1.0,
+                gridLines: {
+                  color: 'transparent',
+                },
+                ticks: {
+                  fontColor: '#ecebe8',
+                  fontSize: 16,
+                },
+              }],
+              xAxes: [{
+                display: false,
+              }]
+            },
+            plugins: {
+              datalabels: {
+                align: 'end',
+                anchor: 'end',
+                color: '#ecebe8',
+                formatter: function (value) {
+                  return Math.round(value) + '%';
+                },
+                font: {
+                  size: 16,
+                }
+              }
+            }
+          }}
+            />
         </div>
       </div>
     );
