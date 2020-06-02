ddad331c8490bddcaf9e96945ac73a1400366e90
Sohini Mukherjee <s23mukhe@uwaterloo.ca>
2020-06-02 01:31:16 -0400

    Updates

---
+++ precache-manifest.0f4a797f9d2c71a81ca5b6f2ba11cc29.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2e30fded758dcdd684bb9a0dcb77924",
+    "url": "/SpotifySocialApp/index.html"
+  },
+  {
+    "revision": "bb831036ea8e3ceb0607",
+    "url": "/SpotifySocialApp/static/css/main.5487c9ff.chunk.css"
+  },
+  {
+    "revision": "de59588dd521d544cae6",
+    "url": "/SpotifySocialApp/static/js/2.9619a686.chunk.js"
+  },
+  {
+    "revision": "5549393ced9478226ddab7416c8b2c72",
+    "url": "/SpotifySocialApp/static/js/2.9619a686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb831036ea8e3ceb0607",
+    "url": "/SpotifySocialApp/static/js/main.d03ca169.chunk.js"
+  },
+  {
+    "revision": "823cf6eff881d99e76ab",
+    "url": "/SpotifySocialApp/static/js/runtime-main.b2d63eb9.js"
+  },
+  {
+    "revision": "c3ad7d00e32b3af74c11dd852a542c07",
+    "url": "/SpotifySocialApp/static/media/add-a-buddy.c3ad7d00.png"
+  },
+  {
+    "revision": "56c7c30a38eb5cdb02061121da30e6ad",
+    "url": "/SpotifySocialApp/static/media/buddies.56c7c30a.png"
+  },
+  {
+    "revision": "cdd28c5e45e51669c9b5d46a55a1f204",
+    "url": "/SpotifySocialApp/static/media/buddy-requests.cdd28c5e.png"
+  },
+  {
+    "revision": "7a34cf4279c6f8f8c97a1250ccce40a4",
+    "url": "/SpotifySocialApp/static/media/logo-green.7a34cf42.png"
+  }
+]);
