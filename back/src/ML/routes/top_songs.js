3b7f830cacad721e3a19133e11c1fa63a82d2f31
SIDHAANT ANAND <S29ANAND@uwaterloo.ca>
2020-12-02 22:10:00 -0500

    Changed top ML API

--- back/src/ML/routes/top_songs.js
+++ back/src/ML/routes/top_songs.js
@@ -4,23 +4,9 @@ module.exports = async function(req,res,constants,request,helpers) {
 		const allData = {};
 		for(var user_index in data) {
 			var key = req.session.ids[user_index];
-			var access_token = req.session.access_tokens[user_index];
-			var curr_user_data = [];
-			for(var song of data[user_index].items) {
-				var curr_song = {};
-				curr_song['id'] = song['id'];
-				curr_song['artists'] = song['artists'].map(artist => artist.id);
-				curr_song['genres'] = [];
-				for(var artist_id of curr_song['artists']) {
-					var artist_info = await helpers.fetchArtistInfo(constants,request,access_token,artist_id);
-					curr_song['genres'].push(artist_info.genres);
-				}
-				curr_user_data.push(curr_song);
-			}
-
-			allData[key] = curr_user_data;
+			var song_ids = data[user_index].items.map(song => song['id'])
+			allData[key] = song_ids
 		}
-
 		res.status(200).send(allData);
 		return;
 	}, reject => {
