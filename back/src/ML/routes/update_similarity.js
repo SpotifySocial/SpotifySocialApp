0beab495dbfeea2e8ed381933fc3dd4e210ed0ec
SIDHAANT ANAND <S29ANAND@uwaterloo.ca>
2021-01-05 00:47:22 -0500

    Syntax changes

--- back/src/ML/routes/update_similarity.js
+++ back/src/ML/routes/update_similarity.js
@@ -2,37 +2,37 @@ module.exports = function(req,res,constants,client,helpers) {
 	helpers.fetchUserSimilarity(constants,{},client).then(users => {
 		const db_ids = users.map(user => user._id);
 		const promises = [];
-		for(var curr of req.body.data) {
+		for(let curr of req.body.data) {
 			const id1 = curr.user1;
 			const id2 = curr.user2;
 			const similarity = curr.cosine_similarity;
 
-			var friends_id1 = [];
-			var similarity_id1 = [];
-			var friends_id2 = [];
-			var similarity_id2 = [];
+			let friends_id1 = [];
+			let similarity_id1 = [];
+			let friends_id2 = [];
+			let similarity_id2 = [];
 
-			if(db_ids.indexOf(id1) != -1) {
+			if(db_ids.indexOf(id1) !== -1) {
 				friends_id1 = users[db_ids.indexOf(id1)].friends
 				similarity_id1 = users[db_ids.indexOf(id1)].similarity
 			}
 
-			if(db_ids.indexOf(id2) != -1) {
+			if(db_ids.indexOf(id2) !== -1) {
 				friends_id2 = users[db_ids.indexOf(id2)].friends
 				similarity_id2 = users[db_ids.indexOf(id2)].similarity
 			}
 
-			var index_id1 = -1;
-			var index_id2 = -1;
+			let index_id1 = -1;
+			let index_id2 = -1;
 
-			for(var i in friends_id1) {
+			for(let i in friends_id1) {
 				if(friends_id1[i] == id2){
 					index_id1 = i;
 					break;
 				}
 			}
 
-			for(var i in friends_id2) {
+			for(let i in friends_id2) {
 				if(friends_id2[i] == id1){
 					index_id2 = i;
 					break;
